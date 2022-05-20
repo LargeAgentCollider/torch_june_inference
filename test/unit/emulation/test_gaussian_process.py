0f1a508efcb3b5a853ecf90beb81005266b1405b
Arnau Quera-Bofarull <arnauq@protonmail.com>
2022-05-20 14:14:38 +0100

    gaussian emulation simple tests

---
+++ test/unit/emulation/test_gaussian_process.py
@@ -0,0 +1,42 @@
+import pytest
+import torch
+import numpy as np
+
+from torch_june_inference.emulation import GPEmulator
+
+
+class TestGPEmulation:
+    @pytest.fixture(name="data")
+    def make_data(self):
+        xs = torch.tensor([[0,0,0.], [1,1,1.]])
+        ys = 2 * xs + 1
+        return xs, ys
+
+    @pytest.fixture(name="emulator")
+    def build_emulator(self, data):
+        xs, ys = data
+        return GPEmulator(train_x=xs, train_y=ys)
+
+    def test__emulation(self, data, emulator):
+        emulator.train_emulator(max_training_iter=1000)
+        emulator.set_eval()
+        xs, ys = data
+        # test inputs
+        with torch.no_grad():
+            for i in range(2):
+                res = emulator(xs[i].reshape(1,-1))
+                mean = res.mean
+                lower, upper = res.confidence_region()
+                assert (lower < ys[i]).all()
+                assert (upper > ys[i]).all()
+
+        # test interpolation
+        xs = torch.tensor([[0.3, 0.2, 0.4], [0.5, 0.6, 0.7]])
+        ys = 2 * xs + 1
+        with torch.no_grad():
+            for i in range(2):
+                res = emulator(xs[i].reshape(1,-1))
+                mean = res.mean
+                lower, upper = res.confidence_region()
+                assert (lower < ys[i]).all()
+                assert (upper > ys[i]).all()
---
+++ torch_june_inference/emulation/gaussian_process.py
@@ -0,0 +1,61 @@
+import torch
+from tqdm import tqdm
+import numpy as np
+import gpytorch
+
+
+class GPEmulator(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, likelihood=None, device="cpu"):
+        n_tasks = train_y.shape[-1]
+        if likelihood is None:
+            likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(
+                num_tasks=n_tasks
+            )
+        super(GPEmulator, self).__init__(train_x, train_y, likelihood)
+        self.likelihood = likelihood.to(device)
+        self.mean_module = gpytorch.means.MultitaskMean(
+            gpytorch.means.ZeroMean(), num_tasks=n_tasks
+        )
+        self.covar_module = gpytorch.kernels.MultitaskKernel(
+            gpytorch.kernels.RBFKernel(), num_tasks=n_tasks, rank=1
+        )
+        self.train_x = train_x
+        self.train_y = train_y
+
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultitaskMultivariateNormal(mean_x, covar_x)
+
+    def train_emulator(self, optimizer=None, max_training_iter=100):
+        self.train()
+        self.likelihood.train()
+        if optimizer is None:
+            optimizer = torch.optim.Adam(self.parameters(), lr=0.1)
+        mll = gpytorch.mlls.ExactMarginalLogLikelihood(self.likelihood, self)
+        previous_loss = np.inf
+        pbar = tqdm(range(max_training_iter))
+        for i in pbar:
+            # Zero gradients from previous iteration
+            optimizer.zero_grad()
+            # Output from model
+            output = self(self.train_x)
+            # Calc loss and backprop gradients
+            loss = -mll(output, self.train_y)
+            if i >= max_training_iter:
+                break
+            loss.backward()
+            pbar.set_description(f"{i}: {loss:.3e}\t {previous_loss:.3e}")
+
+            optimizer.step()
+            if i % 100 == 0:
+                if loss < previous_loss:
+                    previous_loss = loss
+                else:
+                    break
+    def set_eval(self):
+        self.eval()
+        self.likelihood.eval()
+
+    def save(self, path):
+        torch.save(self.state_dict(), path)
