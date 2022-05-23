0f77462d5b8ddcee80ddbc89bfa65ad890860b2c
arnauq <arnau.quera@gmail.com>
2022-05-23 13:33:58 +0100

    multinest cube is annoying

--- torch_june_inference/inference/multinest.py
+++ torch_june_inference/inference/multinest.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import pymultinest
+from pymultinest.solve import solve
 import yaml
 import torch
 import numpy as np
@@ -24,41 +25,55 @@ class MultiNest(InferenceEngine):
         TODO: Need to invert from unit cube for other distros.
         """
         for i in range(ndim):
-            cube[i] = cube[i] * 1.5 - 1
+            cube[i] = cube[i] - 1.0
+        return cube
 
     def _loglike(self, cube, ndim, nparams):
         # Set model parameters
+        n_agents = self.runner.n_agents
         with torch.no_grad():
             state_dict = self.runner.model.state_dict()
             for i, key in enumerate(self.priors):
                 state_dict[key].copy_(torch.tensor(cube[i], device=self.device))
+            print(self.runner.model.state_dict())
             # Run model
             self.runner.run()
-        # Compare to data
-        y = self.runner.results[self.data_observable][self.time_stamps]
-        y_obs = self.observed_data[self.time_stamps]
-        return self.likelihood(y).log_prob(y_obs).sum().cpu().item()
+            # Compare to data
+            y = self.runner.results[self.data_observable][self.time_stamps] / n_agents
+            y_obs = self.observed_data[self.time_stamps] / n_agents
+            return self.likelihood(y).log_prob(y_obs).sum().cpu().item()
 
     def run(self, **kwargs):
-        ndim = len(self.priors)
+        ndims = len(self.priors)
+        #result = solve(
+        #    LogLikelihood=self._loglike,
+        #    Prior=self._prior,
+        #    n_dims=ndims,
+        #    outputfiles_basename=(self.results_path / "multinest").as_posix(),
+        #    verbose=True,
+        #    n_iter_before_update=1,
+        #    resume=False,
+        #    **kwargs
+        #)
+
         pymultinest.run(
-            self._loglike,
-            self._prior,
-            ndim,
-            outputfiles_basename=(self.results_path / "multinest").as_posix(),
-            verbose=True,
-            resume=False,
-            n_iter_before_update=1,
-            **kwargs
+           self._loglike,
+           self._prior,
+           ndims,
+           outputfiles_basename=(self.results_path / "multinest").as_posix(),
+           verbose=True,
+           resume=False,
+           n_iter_before_update=1,
+           **kwargs
         )
         self.results = self.save_results()
 
     def save_results(self):
         results = read_fortran_data_file(self.results_path / "multinest.txt")
         df = pd.DataFrame()
-        df["likelihood"] = results[:,1]
+        df["likelihood"] = results[:, 1]
         for i, name in enumerate(self.priors):
-            df[name] = results[:,2+i]
-        df["weights"] = results[:,0]
+            df[name] = results[:, 2 + i]
+        df["weights"] = results[:, 0]
         df.to_csv(self.results_path / "results.csv")
         return df
